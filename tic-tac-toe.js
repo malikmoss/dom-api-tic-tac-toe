7b196a22d1ce74aa4b71bd5ad3f62cb292890a35
Cedric Malik Moss <malik@govalo.com>
2021-12-31 23:46:33 -0500

    current player symbols added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,2 +1,27 @@
-let currentpLAYERsYMBOL = "X"
+let currentPlayerSymbol = "X"
 let squareValues =  ["","","","","","","","",""]
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('tic-tac-toe-board').addEventListener('click', event => {
+        console.log('A square is clicked:', e.target.id)
+        const targetId = e.target.id;
+
+        if(!target.startsWith('square-')) return;
+
+        const squareIndex = Number.parseInt(targetId[targetId.length - 1])
+
+        if (squareValues[squareIndex] !== '') return;
+
+        const img = document.createElement('img');
+        img.src = `../player-${currentPlayerSymbol}.svg`
+        e.target.appendChild(img)
+
+        squareValues[squareIndex] = currentPlayerSymbol;
+
+        if (currentpLAYERsYMBOL ==='X') {
+            currentPlayerSymbol = 'O';
+        } else {
+            currentPlayerSymbol = 'X'
+        }
+    })
+});
